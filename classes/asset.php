7156159690e6fece6d80247d8c620495e43533cb
Jonathan Geiger <jonathan@jonathan-geiger.com>
2009-11-04 23:15:57 -0500

    Now this is really the initial commit

---
+++ classes/asset.php
@@ -0,0 +1,96 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Simple asset helper. Outputs javascript tags for multiple asset 
+ * files, automatically concatenates them into one file as well.
+ * 
+ * Current only javascript tags are supported. Asset hosts, JSMin, and CSS
+ * support are definitely planned. 
+ *
+ * @package default
+ * @author Jonathan Geiger
+ */
+class Asset
+{
+	/**
+	 * @param array 	$paths		The paths to generate
+	 * @param string	$prefix 	A string to prefix to the cache file
+	 * @return void
+	 * @author Jonathan Geiger
+	 */
+	public static function javascripts(array $paths, $prefix)
+	{
+		$config = Kohana::config('assets');
+
+		// Lots of paths to deal with here
+		$base_dir = $config['javascripts'];
+		$cache_dir = $config['javascripts_cache'];
+		$cache = $config['cache'];
+		
+		// Initialize some variables
+		$all = '';
+		$greatest = 0;
+		$written = 0;
+			
+		// Check if the cache needs to be renewed
+		foreach ($paths as $path)
+		{	
+			$time = filemtime(DOCROOT.$base_dir.$path);
+					
+			// Gather last modified time
+			if ($time > $greatest)
+			{
+				$greatest = $time;
+			}
+			
+			// Create the scripts
+			$all .= html::script($base_dir.$path.'?'.$time)."\n";
+		}
+		
+		// Now that we've found the cache path, we can just concatenate it all together
+		$cache_file = $cache_dir.$prefix.$greatest.'.js';
+		
+		// Check the cache
+		if ($cache && file_exists(DOCROOT.$cache_file))
+		{
+			return html::script($cache_file);
+		}
+		
+		// Still here? Do we need to cache? 
+		if ($cache && asset::cache($paths, $cache_file))
+		{
+			return html::script($cache_file);
+		}
+		else
+		{
+			return $all;
+		}
+	}
+	
+	protected static function cache($files, $output)
+	{
+		$written = 0;
+		
+		// Hide errors
+		$ERR = error_reporting(0);
+		
+		$file = fopen($output, 'w+');
+		
+		// That sucks. Another loop
+		foreach ($paths as $path)
+		{						
+			// Write to the cache
+			if ($file)
+			{
+				$written += fwrite($file, file_get_contents(DOCROOT.$prefix.$path));
+			}
+		}
+		
+		// Done here
+		fclose($output);
+		error_reporting($ERR);
+		
+		return $written;
+	}
+	
+} // END class Asset
---
+++ config/assets.php
@@ -0,0 +1,31 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	
+	/**
+	 * Javascript directory to prefix to all generated 
+	 * files (except cache files...see below).
+	 * 
+	 * url::base() is prepended to this automatically.
+	 */
+	'javascripts' => 'javascripts/',
+	
+	/**
+	 * Cache directory for javascripts
+	 * 
+	 * ex. javascripts/cache-	writes to	javascripts/cache-212985812.js
+	 * ex. javascripts/cache/	writes to	javascripts/cache/212985812.js
+	 * 
+	 * url::base() is prepended to this automatically.
+	 */ 
+	'javascripts_cache' => 'javascripts/cache/',
+
+	/**
+	 * Whether or not to cache. Cache expiration is handled 
+	 * automatically, based on whether or not a file has changed.
+	 * 
+	 * Garbage collection, is not currently handled by the system
+	 */
+	'cache'	=> (Kohana::$environment == 'production') ? TRUE : FALSE
+	
+);
