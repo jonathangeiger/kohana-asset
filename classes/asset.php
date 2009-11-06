01951d1899ad5ceac2b61db79ae0a0f954775ad5
Jonathan Geiger <jonathan@jonathan-geiger.com>
2009-11-05 23:10:07 -0500

    Major API and Configuration changes
      * Added support for stylesheets
      * Opened up a few methods after refactoring a bit
      * Cached files are now stored in `cache/` instead of inside a `javascripts/cache` or similar folder. This can be changed, if necessary.
      * Reorganized configuration layout
      * Improved internal documentation

--- classes/asset.php
+++ classes/asset.php
@@ -12,83 +12,214 @@
  */
 class Asset
 {
+	const JAVASCRIPT = 'javascripts';
+	const STYLESHEET = 'stylesheets';
+	const CACHE = 'cache';
+	
 	/**
+	 * Generates javascript tags based on the paths provided. 
+	 * Caches automatically. File extensions (.js) are optional.
+	 *  
 	 * @param array 	$paths		The paths to generate
 	 * @param string	$prefix 	A string to prefix to the cache file
 	 * @return void
 	 * @author Jonathan Geiger
 	 */
 	public static function javascripts(array $paths, $prefix = NULL)
-	{
-		$config = Kohana::config('assets');
-
-		// Lots of paths to deal with here
-		$base_dir = $config['js_dir'];
-		$cache_dir = $config['js_cache_dir'];
-		$cache = $config['cache'];
+	{		
+		$scripts = '';
 		
-		// Initialize some variables
-		$all = '';
+		// Just loop through each one and output it
+		foreach(asset::paths($paths, $prefix, asset::JAVASCRIPT) as $path)
+		{
+			$scripts .= html::script($path)."\n";
+		}
+		
+		return $scripts;
+	}
+	
+	/**
+	 * @param array 	$paths		The paths to generate
+	 * @param string	$prefix 	A string to prefix to the cache file
+	 * @return void
+	 * @author Jonathan Geiger
+	 */
+	public static function stylesheets(array $paths, $prefix = NULL)
+	{		
+		$scripts = '';
+		
+		// Just loop through each one and output it
+		foreach(asset::paths($paths, $prefix, asset::STYLESHEET) as $path)
+		{
+			$scripts .= html::style($path)."\n";
+		}
+		
+		return $scripts;
+	}
+	
+	/**
+	 * Creates a full path for a particular file, taking into account things such as relative/absolute paths
+	 *
+	 * @param array 	$path 		The path to fix
+	 * @param string 	$type 		The type of file we're working with
+	 * @return void
+	 * @author Jonathan Geiger
+	 */
+	public static function path($path, $type)
+	{	
+		// Find the prefix and extension
+		$prefix = asset::config($type, 'prefix');
+		$extension = asset::config($type, 'extension');
+		
+		// Append the prefix only if it's a relative path
+		if (substr($path, 0, 1) != '/')
+		{
+			$path = $prefix.$path;
+		}
+		
+		// Ensure we have a file extension (except caches)
+		if ($type !== asset::CACHE && $extension !== substr($path, -strlen($extension)))
+		{
+			$path = $path.$extension;
+		}
+				
+		return $path;
+	}
+	
+	/**
+	 * Determines the paths to be used for a set of asset includes.
+	 *
+	 * @param array 	$paths 		The paths to generate tags for
+	 * @param string 	$prefix 	A prefix to add to any generated cache files
+	 * @param string 	$type 		The type of file we're working with
+	 * @return void
+	 * @author Jonathan Geiger
+	 */
+	public static function paths(array $paths, $prefix, $type)
+	{
 		$greatest = 0;
-		$written = 0;
+		$docroot = asset::config('root');
+		$count = count($paths);
 			
 		// Check if the cache needs to be renewed
-		foreach ($paths as $path)
-		{	
-			$time = filemtime(DOCROOT.$base_dir.$path);
-					
-			// Gather last modified time
-			if ($time > $greatest)
+		for ($i = 0; $i < $count; $i++)
+		{			
+			$path = asset::path($paths[$i], $type);
+				
+			// Gather last modified time, the greatest of which will 
+			// be used in the filename of the cache file	
+			if (($time = @filemtime($docroot.$path)) > $greatest) 
 			{
 				$greatest = $time;
 			}
 			
-			// Create the scripts
-			$all .= html::script($base_dir.$path.'?'.$time)."\n";
+			// Create the scripts, which may be discarded altogether 
+			// if the cache file is present
+			$paths[$i] = $path."?".$time;
 		}
 		
-		// Now that we've found the cache path, we can just concatenate it all together
-		$cache_file = $cache_dir.$prefix.$greatest.'.js';
+		// Now that we've found the cache path, we can 
+		// just concatenate it all together
+		$cache = asset::path($prefix.$greatest, asset::CACHE).asset::config($type, 'extension');
 		
 		// Check the cache
-		if ($cache && file_exists(DOCROOT.$cache_file))
+		if (asset::cached($docroot.$cache, $type) || asset::cache($paths, $type, $cache))
 		{
-			return html::script($cache_file);
-		}
-		
-		// Still here? Do we need to cache? 
-		if ($cache && asset::cache($paths, $cache_file))
-		{
-			return html::script($cache_file);
+			return array($cache);
 		}
 		else
 		{
-			return $all;
+			return $paths;
 		}
 	}
 	
-	protected static function cache($files, $output)
+	/**
+	 * Simple accessor for configuration properties
+	 *
+	 * @param string 	$group  The group (main array key) to find
+	 * @param string 	$key 	The key (sub-array key) to retrieve
+	 * @return void
+	 * @author Jonathan Geiger
+	 */
+	protected static function config($group = NULL, $key = NULL)
 	{
-		$written = 0;
+		static $config = NULL;
 		
-		// Hide errors
-		$ERR = error_reporting(0);
-		
-		$file = fopen($output, 'w+');
-		
-		// That sucks. Another loop
-		foreach ($paths as $path)
-		{						
-			// Write to the cache
-			if ($file)
-			{
-				$written += fwrite($file, file_get_contents(DOCROOT.$prefix.$path));
-			}
+		// First time, load that config
+		if ($config === NULL)
+		{
+			$config = Kohana::config('assets');
 		}
 		
-		// Done here
-		fclose($output);
-		error_reporting($ERR);
+		if ($group === NULL)
+		{
+			return $config;
+		}
+		else if ($key === NULL)
+		{
+			return $config[$group];
+		}
+		else
+		{
+			return $config[$group][$key];
+		}		
+	}
+	
+	/**
+	 * Determines whether or not a file is cached and 
+	 * the system is accepting cached files
+	 *
+	 * @param string 	$file 	The file to check is cached
+	 * @param string 	$type  	The type of file we're working with
+	 * @return void
+	 * @author Jonathan Geiger
+	 */
+	protected static function cached($file, $type)
+	{
+		return (asset::config($type, 'cache') && file_exists($file));
+	}
+	
+	/**
+	 * Caches a group of files
+	 *
+	 * @param string 	$paths 	The paths to cache
+	 * @param string 	$type  	The type of file we're working with
+	 * @param string 	$cache 	A path to the file to concatenate $paths to
+	 * @return mixed
+	 * @author Jonathan Geiger
+	 */
+	protected static function cache($paths, $type, $cache)
+	{
+		// Make sure we're allowed to cache
+		if (!asset::config($type, 'cache')) return FALSE;
+		
+		// The total number of bytes written to the file
+		$written = 0;
+		
+		// Open that file!
+		$cache = fopen($cache, 'w+');
+				
+		if ($cache)
+		{
+			// That sucks. Another loop
+			foreach ($paths as $path)
+			{						
+				$path = asset::config('root').$path;	
+				
+				// Paths could potentially have a cachebuster on 
+				// the end of them. Make sure to strip it.
+				if ($pos = strpos($path, '?'))
+				{
+					$path = substr($path, 0, $pos);
+				}
+
+				// Write to the cache
+				$written += fwrite($cache, file_get_contents($path)."\n");
+			}
+			
+			// Done here
+			fclose($cache);
+		}
 		
 		return $written;
 	}
--- config/assets.php
+++ config/assets.php
@@ -3,29 +3,92 @@
 return array(
 	
 	/**
-	 * Javascript directory to prefix to all generated 
-	 * files (except cache files...see below).
+	 * The location on the filesystem to the paths below. 
 	 * 
-	 * url::base() is prepended to this automatically.
+	 * This is assumed to include url::base() and end with a trailing slash.
 	 */
-	'js_dir' => 'javascripts/',
+	'root' => DOCROOT,
 	
 	/**
-	 * Cache directory for javascripts
-	 * 
-	 * ex. javascripts/cache-	writes to	javascripts/cache-212985812.js
-	 * ex. javascripts/cache/	writes to	javascripts/cache/212985812.js
-	 * 
-	 * url::base() is prepended to this automatically.
-	 */ 
-	'js_cache_dir' => 'javascripts/cache/',
+	 * Config for calls to asset::javascripts()
+	 */
+	'javascripts' => array(
+		
+		/**
+		 * Javascript directory to prefix to all generated 
+		 * files (except cache files...see below).
+		 * 
+		 * url::base() is prepended to this automatically.
+		 */
+		'prefix' => 'javascripts/',
+		
+		/**
+		 * Should be self-explanatory
+		 */
+		'extension' => '.js',
 
-	/**
-	 * Whether or not to cache. Cache expiration is handled 
-	 * automatically, based on whether or not a file has changed.
-	 * 
-	 * Garbage collection, is not currently handled by the system
-	 */
-	'cache'	=> (Kohana::$environment == 'production') ? TRUE : FALSE
+		/**
+		 * Whether or not to cache. Cache expiration is handled 
+		 * automatically, based on whether or not a file has changed.
+		 * 
+		 * Garbage collection is not currently handled by the system
+		 */
+		'cache'	=> TRUE
+	),
 	
+	/**
+	 * Config for calls to asset::stylesheets()
+	 */
+	'stylesheets' => array(
+		
+		/**
+		 * Javascript directory to prefix to all generated 
+		 * files (except cache files...see below).
+		 * 
+		 * url::base() is prepended to this automatically.
+		 */
+		'prefix' => 'stylesheets/',
+		
+		/**
+		 * Should be self-explanatory
+		 */
+		'extension' => '.css',
+
+		/**
+		 * Whether or not to cache. Cache expiration is handled 
+		 * automatically, based on whether or not a file has changed.
+		 * 
+		 * Garbage collection is not currently handled by the system
+		 */
+		'cache'	=> TRUE
+	),
+	
+	/**
+	 * Config for cached files
+	 */
+	'cache' => array(
+		/**
+		 * Cache directory for all cached files. Note that javascript AND css
+		 * files will be prefixed with this. 
+		 * 
+		 * This may seem a bit "unclean" but it's ultimately better: there's 
+		 * only one directory to delete if you want to manually expire the 
+		 * cache. 
+		 * 
+		 * Also, stylesheets won't have any problems with url() references
+		 * if you keep it at the same level as your stylesheets directory.
+		 * If cache files were cached in the stylesheets directory, it would 
+		 * get all cluttered with source files and cached files.
+		 * 
+		 * url::base() is prepended to this automatically when the 
+		 * script/link tag is generated.
+		 */ 
+		'prefix' => 'cache/',
+		
+		/**
+		 * Note that this is slightly different from the actual file type extensions.
+		 * Just use it if you want to append something to the cache filename.
+		 */
+		'extension' => NULL
+	)
 );
